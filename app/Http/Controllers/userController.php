4887c93e4f1bbe36524a0cf1f3df4f9b7cde200a
codingfaf <rahaf.kn@gmail.com>
2023-06-11 19:33:55 +0300

    register error fixed

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -28,7 +28,7 @@ class userController extends Controller
             'password' => 'required|string|min:6',
         ]);
         if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
+            return response()->json($validator->errors(), 400);
         }
         $user = User::create(
             array_merge(
